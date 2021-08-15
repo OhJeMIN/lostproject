d93b2aa75da34b8b65ed67b2865faf0757444cfe
jasonoh22 <jasonoh22@naver.com>
2021-08-15 18:05:14 +0900

    passport login logout

--- app.js
+++ app.js
@@ -2,72 +2,148 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var crypto = require('crypto')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var AccountRouter = require('./routes/Account');
-var DashboardRouter = require('./routes/Dashboard');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const AccountRouter = require('./routes/Account');
+const DashboardRouter = require('./routes/Dashboard');
+const bodyParser = require('body-parser');
+var jwt = require('jsonwebtoken');
+
+require('dotenv').config();
+// var decoded_data = jwt.verify(token, 'secret_key');
+// console.log(decoded_data.sub)
 
 var authData = {
   username: 'jasonoh2',
   password: '1111'
 }
 
+const mysql = require('mysql');
+const { response, json } = require('express');
+
+const con= mysql.createConnection({
+  host: '3.143.232.77',
+  user:'jasonoh22',
+  password: '1234',
+  database: 'RAProject'
+});
+
 
 var app = express();
 const passport = require('passport')
 , LocalStrategy = require('passport-local').Strategy;
-app.use(passport.initialize());
-app.use(passport.session());
+const router = require('./routes/index');
 
 
 passport.serializeUser(function(user, done) {
+  console.log(3);
   done(null, user.username);
 });
 
 passport.deserializeUser(function(id, done) {
+  console.log(4);
   User.findById(id, function(err, user) {
     done(err, user);
   });
 });
 
+function hash(password) {
+  return crypto.createHash('sha512').update(password).digest('hex');
+};
+
+function validatePassword (password, pass_hash) {
+  // 함수로 전달받은 password 의 해시값과, 데이터에 담겨있는 해시값과 비교를 합니다.
+  const hashed = hash(password);
+  return pass_hash === hashed;
+};
+
+//패스포트 로컬전략 설정
 passport.use(new LocalStrategy(
-  function(username, password, done) {
-    console.log('LocalStrategy', username, password);
-    if(username === authData.username){
-      console.log(1);
-      if(password === authData.password){
-        console.log(2);
-        return done(null, user);
-      }else{
-        console.log(3);
-        return done(null, false, { message: 'Incorrect password.' });
+  {
+  usernameField: "user_id",
+
+  passwordField: "user_password"
+  },  
+  async function(username, password, done) {
+    //사용자가 코드를 작성
+    //우리는 mysql 쓰니까 mysql에서 정보를 받아와서 받은 정보하고 일치하는지 확인        
+    var sql="select User_id,password_hash from User where User_id=?"
+    var id = [username];
+    passhash = String(hash(password));
+    
+    con.query(sql, id, function(err, result){
+      if(err){
+        return done(null, false, {message: "id not found"});
+      }      
+      else{
+        if(passhash === result[0].password_hash){         
+          var token=jwt.sign({
+            username: "id",
+            password: "password"
+          },
+          "process.env.SECRET_KEY",
+          {
+            subject: "LostProject jwtToken",
+            expiresIn: '60m',
+            issuer: "id"
+          });
+          result[0]["Token"] = token; // 생성한 토큰을 result에다가 넣어주기
+          done(null, result);
+        }
+        else{
+          return done(null, false, {message: "password wrong"});
+        }
       }
-    }else{
-      console.log(4);
-      return done(null, false, { message: 'Incorrect username.' });
-    }   
+    })
   }
 ));
 
-router.post('/Account/signup',
-  passport.authenticate('local',   //local은 유저이름, 비번으로 로그인 / local이 아닌것은 facebook or google 등등
-));
-
-const mysql = require('mysql');
-const { response } = require('express');
-
-const con= mysql.createConnection({
-  host: '3.143.232.77',
-  user:'jasonoh22',
-  password: '1234'
+app.use(bodyParser.json());
+//로그인 시키는 
+// 패스포트 실행 -> 로그인(로컬)
+app.post('/Account/signin', async function(req, res, next){
+  passport.authenticate('local',(err ,user, info)=>{    
+    if(err){
+      console.error(err);
+    }
+    if(!user){
+      return res.json(info);
+    }
+    try{
+      var check =jwt.verify(user[0]["Token"], "process.env.SECRET_KEY");
+      if(check){
+        console.log("success");
+        return res.json({user});
+      }
+    }
+    catch(err){
+      console.log(err);
+      return res.json("token wrong");
+    }
+  }
+)(req,res,next);
 });
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log('Connected');
+app.post('/Account/state', function(req, res) {
+  if(verifyJsonWebToken(request_token)) {
+    doApiJob();
+} else {
+    res.send('token is invalid')
+}
+});
+
+app.get('/logout', function(req,res){
+  req.logout();
+  req.session.save(function(){
+    res.redirect('/');
   });
-  
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.get('/', (req, res) => {  
   con.query('SHOW databases;', function(err, result) {
     if (err) throw err;
--- routes/Account.js
+++ routes/Account.js
@@ -1,22 +1,75 @@
 var express = require('express');
 var router = express.Router();
+const crypto = require('crypto');
+const mysql = require('mysql');
 
 router.use(function(req, res, next){
     next();
 });
+
+const con= mysql.createConnection({
+    host: '3.143.232.77',
+    user:'jasonoh22',
+    password: '1234',
+    database: 'RAProject'
+  });
+  
+  //비밀번호 해싱(암호화)
+function hash(password) {
+    return crypto.createHash('sha512').update(password).digest('hex');
+};
+
+function validatePassword (password, pass_hash) {
+    // 함수로 전달받은 password 의 해시값과, 데이터에 담겨있는 해시값과 비교를 합니다.
+    const hashed = hash(password);
+    return pass_hash === hashed;
+};
+
 /* GET users listing. */
-router.post('/', function(req, res, next) {
+//async 넣으면 동기 
+router.post('/', async function(req, res, next) {
   res.status(201).json('Account');
 });
 
-router.post('/signup', function(req, res, next){
-    username= req.body.user_id;
-    password = req.body.user_password;
-    type = req.body.signup_type;    
-    res.status(201).json("id:"+username+","+"pw:"+password+","+"type:"+type);  
+//회원가입
+router.post('/signup', async function(req, res, next){
+    try{ 
+        username= req.body.user_id;
+        password = req.body.user_password;      
+        confirm_password = req.body.confirm_password;  
+        passhash = String(hash(password));       
+         if(username.length < 1 ||  password.length < 1 ){
+            res.json('iD나 비밀번호에 공백이 있습니다.');
+            console.log('Blank');
+        }
+        if(confirm_password != password){
+            res.json('비밀번호 검증이 맞지 않습니다');
+            console.log('confirm_password wrong');
+        }
+        else{
+            var idd = [username];
+            con.query("select user_id from User where user_id=?", idd , function(err, data){                
+                if(data[0].user_id.length == 0){                    
+                    con.query("insert into User(user_id, password_hash, login_type) values ('"+username+"','"+passhash+"',"+"1);", function(err, result) {
+                    if (err) throw res.json(err);
+                    res.json('success');
+                    })
+                }
+                else{
+                    res.json('exist ID try another ID');
+                    console.log(err);
+                }
+            })
+        }       
+    }
+    catch(err){
+        res.json('err');
+        console.log(err);
+    }  
 });
 
-router.post('/signin', function(req, res, next){
+
+router.post('/signin', function(req, res, next){    
     res.status(201).json('"messeage" : "success"');
 });
 
